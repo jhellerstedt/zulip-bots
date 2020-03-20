627fedd71b5702fc7f2b0001da12b4d6059fc1cf
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2020-03-20 12:58:15 +1100

    first commit

---
+++ G80 UPS bot/ups-bot.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 15:40:10 2019
+
+@author: jack
+"""
+
+#import os
+
+from typing import Any, Dict
+
+import pickle
+
+from datetime import datetime, timedelta
+import pytz
+
+
+ups_status_file = '/home/jack/zulip_bots/ups_bot/ups_status.p'
+# ups_status_file = os.getcwd() + '/ups_status.p'
+
+class UPSstatus(object):
+    '''
+    This bot loads the ups snmp web server and gets the current status values
+    '''
+    
+    def usage(self) -> str:
+        return '''
+            This bot notifies subscribers if there is a problem with the UPS
+        '''
+    
+    
+    
+    def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+        HELP_STR = (
+                'Commands:'
+                '\n* `@ups-bot help`: display this message'
+                '\n* `@ups-bot status`: fetch current ups status'
+                '\n* `@ups-bot subscribe`: receive @ notifications of problems'
+                '\n* `@ups-bot unsubscribe`: stop receiving @ notifications of problems'
+                '\n* `@ups-bot mute <int>`: stop error notifications for <int> minutes; default 30 (no value given)'
+                )
+        
+        original_content = message['content'].strip()
+        command = original_content.split()
+        
+        if command[0] == 'help' or command[0] == 'Help':
+            bot_handler.send_reply(message, HELP_STR)
+            return
+            
+        
+        elif command[0] == 'status' or command[0] == 'Status':
+            with open(ups_status_file, 'rb') as f:
+                ups_status = pickle.load(f)
+            
+            status_message = ''
+            for ii in ups_status:
+                status_message = status_message + '\n* ' + str(ii) + ': ' + str(ups_status[ii]) + ' '
+            
+            bot_handler.send_reply(message, status_message)
+            
+            try:
+                muted = datetime.now() < datetime.strptime(bot_handler.storage.get('unmute_time'), "%Y-%m-%d %H:%M:%S")
+            except:
+                muted = False
+            
+            if ups_status['ups_problem'] is True and muted is False:
+                msg_dict = dict(
+                        type='stream',
+                        to='spm experiments',
+                        subject='ups status',
+                        content=status_message,
+                        )
+                bot_handler.send_message(msg_dict)
+                
+                for subscriber in bot_handler.storage.get('subscribers'):
+                    msg_dict = dict(
+                        type='private',
+                        to=subscriber,
+                        subject='ups problem',
+                        content=status_message,
+                        )
+                    bot_handler.send_message(msg_dict)
+                    
+                bot_handler.storage.put('error_reported', True)
+            
+            ### send 'all clear' message if power is restored
+            try:
+                answer = bot_handler.storage.get('error_reported')
+            except:
+                bot_handler.storage.put('error_reported', False)
+            
+            if ups_status['ups_problem'] is False and bot_handler.storage.get('error_reported') is True:
+                msg_dict = dict(
+                        type='stream',
+                        to='spm experiments',
+                        subject='ups status',
+                        content=status_message,
+                        )
+                bot_handler.send_message(msg_dict)
+                
+                for subscriber in bot_handler.storage.get('subscribers'):
+                    msg_dict = dict(
+                        type='private',
+                        to=subscriber,
+                        subject='power restored',
+                        content=status_message,
+                        )
+                    bot_handler.send_message(msg_dict)
+                    
+                bot_handler.storage.put('error_reported', False)
+                
+            return
+                
+            
+        elif command[0] == 'subscribe' or command[0] == 'Subscribe':
+            try:
+                subscribers = bot_handler.storage.get('subscribers')
+                subscribers.append(message['sender_email'])
+            except:
+                subscribers = [message['sender_email']]
+            bot_handler.storage.put('subscribers', subscribers)
+            bot_handler.send_reply(message, 'successfully subscribed')
+            return
+            
+        elif command[0] == 'unsubscribe' or command[0] == 'Unsubscribe':
+            try:
+                current_subscribers = bot_handler.storage.get('subscribers')
+                new_subscribers = []
+                for subscriber in current_subscribers:
+                    if subscriber != message['sender_email']:
+                        new_subscribers.append(subscriber)
+                bot_handler.storage.put('subscribers', new_subscribers)
+                bot_handler.send_reply(message, 'unsubscribed')
+            except:
+                pass
+                bot_handler.send_reply(message, 'error in unsubscribe')
+            return
+            
+        elif command[0] == 'mute' or command[0] == 'Mute':
+            try:
+                mute_time = int(command[1])
+            except:
+                mute_time = 30
+            unmute_time = str(datetime.now() + timedelta(minutes=mute_time))[:19]
+            AEDT_unmute_time = str(datetime.now(pytz.timezone('Australia/Melbourne')) + timedelta(minutes=mute_time))[:19]
+            bot_handler.storage.put('unmute_time', unmute_time)
+            reply_msg = 'muted until: ' + AEDT_unmute_time
+            bot_handler.send_reply(message, reply_msg)
+            return
+        
+        elif command[0] == 'update_stream':
+            with open(ups_status_file, 'rb') as f:
+                ups_status = pickle.load(f)
+            
+            status_message = ''
+            for ii in ups_status:
+                status_message = status_message + '\n* ' + str(ii) + ': ' + str(ups_status[ii]) + ' '
+            
+            msg_dict = dict(
+                            type='stream',
+                            to='spm experiments',
+                            subject='ups status',
+                            content=status_message,
+                            )
+            bot_handler.send_message(msg_dict)
+        
+        else:
+            bot_handler.send_reply(message, HELP_STR)
+            return
+        
+handler_class = UPSstatus
---
+++ G80 UPS bot/ups-status.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 11:08:43 2019
+
+@author: jack
+"""
+
+import os
+
+import atexit
+
+import time
+import pickle
+
+import datetime
+from datetime import datetime as dt
+import pytz
+from splinter import Browser
+
+import zulip
+
+ups_status_file = '/home/jack/zulip_bots/ups_bot/ups_status.p'
+
+
+## second python bot to send a message to ups-bot, so its not self-sending
+zulip_config_file = '/home/jack/zulip_bots/ups_bot/python-zuliprc'
+# zulip_config_file = os.getcwd() + '/zuliprc'
+# ups_status_file = os.getcwd() + '/ups_status.p'
+
+client = zulip.Client(config_file=str(zulip_config_file))
+
+
+## address to send message to, .e.g., ups-bot
+ups_bot_address = 'ups-bot-bot@nedchat.imipolex.biz'
+
+with open(zulip_config_file) as f:
+    d = f.readline() # just the [api] bit
+    d = f.readline() # address
+    d = d.strip('email=')
+    d = d.strip('\n')
+    python_bot_address = d
+
+print('sending bot address: ' + python_bot_address)
+
+print('receiving bot address: ' + ups_bot_address)
+
+def get_status_dict(browser):
+    ups_status = {}
+    ups_status['ups_problem'] = False
+        
+    xpaths_to_read = {
+            'upsMode':'//*[@id="upsMode"]',
+            'upsTemp':'//*[@id="upsTemp"]',
+            'warning':'//*[@id="warning"]',
+            'batteryVoltage':'//*[@id="batteryVoltage"]',
+            'batteryCapacity':'//*[@id="batteryCapacity"]',
+            'backupTime':'//*[@id="backupTime"]',
+            'Load level L1':'//*[@id="loadLevel"]',
+            'Load level L2':'//*[@id="loadLevelS"]',
+            'Load level L3':'//*[@id="loadLevelT"]'
+            }
+    try:
+        for variable in xpaths_to_read:
+            ups_status[variable] = browser.find_by_xpath(xpaths_to_read[variable]).first.value
+        
+        ups_status['timestamp'] = str(dt.now(pytz.timezone('Australia/Melbourne')))[:19]
+        
+        if ups_status['upsMode'] != 'Line Mode':
+            ups_status['ups_problem'] = True
+            if ups_status['upsMode'] == '---':
+                ups_status['warning'] = 'website not loaded'
+        if ups_status['warning'] != '':
+            ups_status['ups_problem'] = True
+        if float(ups_status['upsTemp']) > 43:
+            ups_status['ups_problem'] = True
+        if float(ups_status['batteryCapacity']) < 90:
+            ups_status['ups_problem'] = True
+    except:
+        ups_status['timestamp'] = str(dt.now(pytz.timezone('Australia/Melbourne')))[:19]
+        ups_status['warning'] = "site unreachable"
+        ups_status['ups_problem'] = True
+            
+        
+    return ups_status
+    
+def send_error_msg(client):
+    msg = {
+        "type": "private",
+        "to": ups_bot_address,
+        "content": "status"
+    }
+    result = client.send_message(msg)
+    
+    return result
+
+def send_stream_update(client):
+    msg = {
+        "type": "private",
+        "to": ups_bot_address,
+        "content": "update_stream"
+    }
+    result = client.send_message(msg)
+    
+    return result
+    
+
+
+def handle_exit():
+    print('trying to close browser')
+    try:
+        browser.quit()
+    except:
+        pass
+    
+atexit.register(handle_exit)
+
+
+    
+browser = Browser('firefox', headless=True, timeout=15)
+
+url = "http://130.194.160.71/sys_status.html?qpi=10_3/3_1_0"
+
+page_loaded = True
+try:
+    browser.visit(url=url)
+    time.sleep(20)
+except:
+    page_loaded = False
+    print('initial load failed')
+    
+tries = 0
+alert_bot = True
+counter_to_next_alert = 0
+status_ok = False
+
+restart_browser_time = dt.now() + datetime.timedelta(hours=4)
+
+daily_update_time = dt.now() - datetime.timedelta(days=1)
+
+while True:
+    while ((page_loaded is False) and (tries < 3)):
+        try:
+            browser.reload()
+            time.sleep(15)
+            ups_status = get_status_dict(browser)
+            if ups_status['warning'] != 'site unreachable':
+                page_loaded = True
+        except:
+            page_loaded = False
+        
+        counter_to_next_alert = counter_to_next_alert + 15
+        tries = tries + 1
+        
+    tries = 0
+    
+    ups_status = get_status_dict(browser)
+    
+    ## change the ups_problem to True for testing purposes:
+    # ups_status['ups_problem'] = True
+    
+    with open(ups_status_file, 'wb') as f:
+        pickle.dump(ups_status, f)
+
+    if ups_status['ups_problem'] is True and alert_bot is True:
+        print(str(dt.now(pytz.timezone('Australia/Melbourne')))[:19] + ' ups_problem, sending alert message')
+        result = send_error_msg(client)
+        print(result)
+        alert_bot = False
+        counter_to_next_alert = 0
+        status_ok = False
+    
+    if ups_status['ups_problem'] is True and alert_bot is False:
+        print(str(dt.now(pytz.timezone('Australia/Melbourne')))[:19] + ' ups problem, muted')
+        counter_to_next_alert = counter_to_next_alert + 1
+        if counter_to_next_alert > 30:
+            alert_bot = True
+            
+    if ups_status['ups_problem'] is False and status_ok is False:
+        print(str(dt.now(pytz.timezone('Australia/Melbourne')))[:19] + ' power on, sending ok')
+        result = send_error_msg(client)
+        status_ok = True
+        
+    if ups_status['warning'] == 'site unreachable' or ups_status['upsMode'] == '---':
+        page_loaded = False
+    
+    ## send status message 9am every day
+    if dt.now(pytz.timezone('Australia/Melbourne')).hour == 9 and dt.now() - daily_update_time > datetime.timedelta(days=1):
+        result = send_stream_update(client)
+        print(result)
+        daily_update_time = dt.now() - datetime.timedelta(minutes=10)
+    
+    ## restart browser every 24 hours to stop memory leak problem
+    if ups_status['ups_problem'] is False and dt.now() - restart_browser_time > datetime.timedelta(days=1):
+        browser.quit()
+        time.sleep(10) 
+        browser = Browser('firefox', headless=True, timeout=15)
+        url = "http://130.194.160.71/sys_status.html?qpi=10_3/3_1_0"
+        page_loaded = True
+        try:
+            browser.visit(url=url)
+            restart_browser_time = dt.now()
+            time.sleep(20)
+        except:
+            page_loaded = False
+            print('restart failed')
+        
+    
+    time.sleep(15)
